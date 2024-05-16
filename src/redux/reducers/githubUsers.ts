14860828da05cb4cfc52efdcb21d60b5ba400fe5
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2024-05-16 20:51:52 +0100

    refactoring to enums with interfaces

--- src/redux/reducers/githubUsers.ts
+++ src/redux/reducers/githubUsers.ts
@@ -1,3 +1,11 @@
+
+enum ActionType {
+
+    SEARCH_USERS = 'search_users',
+    SEARCH_USERS_SUCCESS="search_users_success",
+    SEARCH_USERS_ERROR = "search_users_error"
+}
+
 interface IGithubUsersState {
   loading: boolean;
   error: string | null;
@@ -5,29 +13,27 @@ interface IGithubUsersState {
 }
 
 interface ISearchAction {
-  type: 'search_users';
+  type: ActionType.SEARCH_USERS;
 }
 
 interface ISearchUsersSuccessAction {
-  type: 'search_users_success';
+  type: ActionType.SEARCH_USERS_SUCCESS;
   payload: string[];
 }
 
 interface ISearchUsersErrorAction {
-  type: 'search_users_error';
+  type: ActionType.SEARCH_USERS_ERROR;
   payload: string;
 }
 
-const reducer = (
-  state: IGithubUsersState,
-  action: ISearchAction | ISearchUsersSuccessAction | ISearchUsersErrorAction
-): IGithubUsersState => {
+type Action = ISearchAction | ISearchUsersSuccessAction | ISearchUsersErrorAction;
+const reducer = (state: IGithubUsersState, action: Action): IGithubUsersState => {
   switch (action.type) {
-    case 'search_users':
+    case ActionType.SEARCH_USERS:
       return { loading: true, error: null, data: [] };
-    case 'search_users_success':
+    case ActionType.SEARCH_USERS_SUCCESS:
       return { loading: false, error: null, data: action.payload };
-    case 'search_users_error':
+    case ActionType.SEARCH_USERS_ERROR:
       return { loading: true, error: action.payload, data: [] };
     default:
       return state;
