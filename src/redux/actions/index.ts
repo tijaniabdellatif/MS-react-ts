1014c824263e82c33305d277a38c3031e86f1f03
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2024-05-16 20:55:23 +0100

    organize files

---
+++ src/redux/actions/index.ts
@@ -0,0 +1,18 @@
+import { ActionType } from "redux/action-types";
+
+ interface ISearchAction {
+    type: ActionType.SEARCH_USERS;
+  }
+  
+   interface ISearchUsersSuccessAction {
+    type: ActionType.SEARCH_USERS_SUCCESS;
+    payload: string[];
+  }
+  
+   interface ISearchUsersErrorAction {
+    type: ActionType.SEARCH_USERS_ERROR;
+    payload: string;
+  }
+
+
+  export type Action = ISearchAction | ISearchUsersSuccessAction | ISearchUsersErrorAction;
--- src/redux/reducers/githubUsers.ts
+++ src/redux/reducers/githubUsers.ts
@@ -1,32 +1,11 @@
-
-enum ActionType {
-
-    SEARCH_USERS = 'search_users',
-    SEARCH_USERS_SUCCESS="search_users_success",
-    SEARCH_USERS_ERROR = "search_users_error"
-}
-
+import { ActionType } from "redux/action-types";
+import { Action } from "redux/actions";
 interface IGithubUsersState {
   loading: boolean;
   error: string | null;
   data: string[];
 }
 
-interface ISearchAction {
-  type: ActionType.SEARCH_USERS;
-}
-
-interface ISearchUsersSuccessAction {
-  type: ActionType.SEARCH_USERS_SUCCESS;
-  payload: string[];
-}
-
-interface ISearchUsersErrorAction {
-  type: ActionType.SEARCH_USERS_ERROR;
-  payload: string;
-}
-
-type Action = ISearchAction | ISearchUsersSuccessAction | ISearchUsersErrorAction;
 const reducer = (state: IGithubUsersState, action: Action): IGithubUsersState => {
   switch (action.type) {
     case ActionType.SEARCH_USERS:
